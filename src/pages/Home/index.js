dbd3de0b7ae2d97832fb7daf0bf78b3ee066f2c2
Otavio <otavioomota@gmail.com>
2020-07-04 00:02:48 -0300

    Formulario do cartao de credito

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { 
+  FiUser, 
+  FiCreditCard, 
+  FiCalendar, 
+  FiLock 
+} from 'react-icons/fi'
 
-import { Container } from './styles'
+import monetiz from '../../assets/monetiz.jpg';
+
+import { 
+  Container, 
+  Header, 
+  CardContainer, 
+  CardInformations, 
+  CardInformationsSecundary 
+} from './styles'
 
 function Home(){
+
+  const [cardHolder, setCardHolder] = useState('');
+  const [cardNumber, setCardNumber] = useState('');
+  const [cardMonthValidity, setCardMonthValidity] = useState('')
+  const [cardYearValidity, setCardYearValidity] = useState('')
+  const [cvc, setCvc] = useState('')
+
   return (
    <Container>
-      <h1>Home</h1>
+      <Header>
+        <img src={monetiz} alt='Monetiz'/>
+        <h2>Monetiz</h2>
+      </Header>
+
+      <CardContainer>
+        <CardInformations>
+            <span>Informações do cartão de crédito</span>
+
+            <div>
+              <FiUser size={18}/>
+              <input 
+                onChange={e => setCardHolder(e.target.value)}  
+                placeholder='Nome no cartão'
+                type='text'
+              />
+            </div>
+
+            <div>
+              <FiCreditCard size={18}/>
+              <input 
+                onChange={e => setCardNumber(e.target.value)}
+                placeholder='Número do cartão'
+                type='text'
+              />
+            </div>
+
+            <CardInformationsSecundary>
+              <div>
+                <FiCalendar size={18}/>
+                <input 
+                  onChange={e => setCardMonthValidity(e.target.value)}
+                  placeholder='Mês'
+                  type='text'
+                />
+              </div>
+
+              <div>
+                <FiCalendar size={18}/>
+                <input 
+                  onChange={e => setCardYearValidity(e.target.value)}
+                  placeholder='Ano'
+                  type='text'
+                />
+              </div>
+
+              <div>
+                <FiLock size={18}/>
+                <input
+                  onChange={e => setCvc(e.target.value)} 
+                  placeholder='Cvc'
+                  type='text'
+                />
+              </div>
+            </CardInformationsSecundary>
+
+        </CardInformations>
+      </CardContainer>
    </Container>
   )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,4 +1,73 @@
 import styled from 'styled-components';
 
 
-export const Container = styled.div``
+export const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  padding: 20px;
+  margin: 0 auto;
+`
+
+export const Header = styled.header`
+  /* background-color: #312E38; */
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+
+  img {
+    height: 56px;
+    width: 56px;
+  }
+`
+
+
+export const CardContainer = styled.div``
+export const CardInformations = styled.form`
+
+  max-width: 400px;
+
+  span {
+    font-size: 18px;
+    font-weight: 500;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    padding: 8px;
+    margin-top: 20px;
+    border-bottom: 1px solid black;
+    
+    input {
+      border: 0;
+      flex: 1;
+      margin-left: 4px;
+    }
+
+    svg {
+      color:#8A2BE2;
+    }
+
+    & + div {
+      margin-top: 14px;
+    }
+  }
+`
+
+export const CardInformationsSecundary = styled.div`
+  display: flex;
+  border: 0 !important;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 !important; 
+  div {
+    margin-top: 0 !important;
+    input {
+      border: 0;
+      flex: 1;
+      margin-left: 4px;
+      width: 50px;
+    }
+  }
+`
